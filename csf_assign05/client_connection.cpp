0ddde3b8f30abed2e7c218b82b27671288ed8cb4
arianashafiee <143358685+arianashafiee@users.noreply.github.com>
2024-12-07 23:29:55 -0500

    .

--- csf_assign05/client_connection.cpp
+++ csf_assign05/client_connection.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <sstream>
+#include <string>
 #include "client_connection.h"
-#include "server.h"
+#include "message.h"
 #include "message_serialization.h"
 #include "exceptions.h"
 
@@ -31,29 +34,29 @@ void ClientConnection::chat_with_client() {
             process_request(request);
         }
     } catch (const InvalidMessage &e) {
-        send_response(Message::MessageType::ERROR, e.what());
+        send_response(MessageType::ERROR, e.what());
     } catch (const CommException &e) {
         m_server->log_error(e.what());
     } catch (const std::exception &e) {
-        send_response(Message::MessageType::ERROR, e.what());
+        send_response(MessageType::ERROR, e.what());
     }
 }
 
 void ClientConnection::process_request(const Message &request) {
-    if (request.get_command() == "LOGIN") {
-        send_response(Message::MessageType::OK, "");
-    } else if (request.get_command() == "BYE") {
-        send_response(Message::MessageType::OK, "");
+    if (request.get_message_type() == MessageType::LOGIN) {
+        // Example implementation for LOGIN
+        send_response(MessageType::OK, "");
+    } else if (request.get_message_type() == MessageType::BYE) {
+        send_response(MessageType::OK, "");
         throw CommException("Client terminated connection");
     } else {
         // Handle other commands like CREATE, PUSH, POP, etc.
-        send_response(Message::MessageType::OK, "");
+        send_response(MessageType::OK, "");
     }
 }
 
-void ClientConnection::send_response(Message::MessageType type, const std::string &content) {
-    Message response(type, content);
-    std::string encoded;
-    MessageSerialization::encode(response, encoded);
+void ClientConnection::send_response(MessageType type, const std::string &content) {
+    Message response(type, {content});
+    std::string encoded = MessageSerialization::encode(response);
     rio_writen(m_client_fd, encoded.c_str(), encoded.size());
 }
