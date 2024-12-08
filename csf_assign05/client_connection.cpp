c11c993c5661b4f23e4bd65a93540778383bb651
arianashafiee <143358685+arianashafiee@users.noreply.github.com>
2024-12-07 23:05:57 -0500

    .

--- csf_assign05/client_connection.cpp
+++ csf_assign05/client_connection.cpp
@@ -7,21 +7,68 @@
 #include "exceptions.h"
 #include "client_connection.h"
 
-ClientConnection::ClientConnection( Server *server, int client_fd )
-  : m_server( server )
-  , m_client_fd( client_fd )
-{
-  rio_readinitb( &m_fdbuf, m_client_fd );
+ClientConnection::ClientConnection(Server *server, int client_fd)
+    : m_server(server), m_client_fd(client_fd) {
+  rio_readinitb(&m_fdbuf, client_fd);
 }
 
-ClientConnection::~ClientConnection()
-{
-  // TODO: implement
+ClientConnection::~ClientConnection() {
+  Close(m_client_fd);
 }
 
-void ClientConnection::chat_with_client()
-{
-  // TODO: implement
+void ClientConnection::chat_with_client() {
+  while (true) {
+    char buffer[Message::MAX_ENCODED_LEN];
+    if (Rio_readlineb(&m_fdbuf, buffer, Message::MAX_ENCODED_LEN) <= 0) break;
+
+    Message request;
+    MessageSerialization::decode(buffer, request);
+    Message response;
+
+    try {
+      handle_request(request, response);
+    } catch (const std::exception &e) {
+      response.set_message_type(MessageType::ERROR);
+      response.push_arg(e.what());
+    }
+
+    std::string encoded_response;
+    MessageSerialization::encode(response, encoded_response);
+    Rio_writen(m_client_fd, encoded_response.c_str(), encoded_response.size());
+  }
 }
 
-// TODO: additional member functions
+void ClientConnection::handle_request(const Message &request, Message &response) {
+  switch (request.get_message_type()) {
+    case MessageType::CREATE:
+      m_server->create_table(request.get_table());
+      response.set_message_type(MessageType::OK);
+      break;
+
+    case MessageType::SET: {
+      Table *table = m_server->find_table(request.get_table());
+      table->lock();
+      table->set(request.get_key(), request.get_value());
+      table->unlock();
+      response.set_message_type(MessageType::OK);
+      break;
+    }
+
+    case MessageType::GET: {
+      Table *table = m_server->find_table(request.get_table());
+      table->lock();
+      std::string value = table->get(request.get_key());
+      table->unlock();
+      response.set_message_type(MessageType::DATA);
+      response.push_arg(value);
+      break;
+    }
+
+    case MessageType::BYE:
+      response.set_message_type(MessageType::OK);
+      throw std::runtime_error("Client disconnected");
+
+    default:
+      throw InvalidMessage("Unknown command");
+  }
+}
--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -1,59 +1,71 @@
 #include <iostream>
-#include <cassert>
 #include <memory>
+#include <map>
+#include <string>
+#include <pthread.h>
 #include "csapp.h"
 #include "exceptions.h"
 #include "guard.h"
 #include "server.h"
 
 Server::Server()
-  // TODO: initialize member variables
+  : m_tables(), m_mutex()
 {
-  // TODO: implement
+  pthread_mutex_init(&m_mutex, nullptr);
 }
 
-Server::~Server()
-{
-  // TODO: implement
+Server::~Server() {
+  pthread_mutex_destroy(&m_mutex);
 }
 
-void Server::listen( const std::string &port )
-{
-  // TODO: implement
+void Server::listen(const std::string &port) {
+  int listenfd = Open_listenfd(port.c_str());
+  if (listenfd < 0) throw CommException("Failed to open listening socket");
+  m_listen_fd = listenfd;
 }
 
-void Server::server_loop()
-{
-  // TODO: implement
+void Server::server_loop() {
+  while (true) {
+    struct sockaddr_storage client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int connfd = Accept(m_listen_fd, (SA *)&client_addr, &client_len);
+    if (connfd < 0) {
+      log_error("Failed to accept connection");
+      continue;
+    }
 
-  // Note that your code to start a worker thread for a newly-connected
-  // client might look something like this:
-
-  //ClientConnection *client = new ClientConnection( this, client_fd );
-  //pthread_t thr_id;
-  //if ( pthread_create( &thr_id, nullptr, client_worker, client ) != 0 )
-    //log_error( "Could not create client thread" );
-
+    ClientConnection *client = new ClientConnection(this, connfd);
+    pthread_t thr_id;
+    if (pthread_create(&thr_id, nullptr, client_worker, client) != 0) {
+      log_error("Failed to create thread");
+      delete client;
+    }
+  }
 }
 
-
-void *Server::client_worker( void *arg )
-{
-  // TODO: implement
-
-  // Assuming that your ClientConnection class has a member function
-  // called chat_with_client(), your implementation might look something
-  // like this:
-
-  //std::unique_ptr<ClientConnection> client( static_cast<ClientConnection *>( arg ) );
-  //client->chat_with_client();
+void *Server::client_worker(void *arg) {
+  std::unique_ptr<ClientConnection> client(static_cast<ClientConnection *>(arg));
+  try {
+    client->chat_with_client();
+  } catch (const std::exception &e) {
+    client->log_error(e.what());
+  }
   return nullptr;
-
 }
 
-void Server::log_error( const std::string &what )
-{
-  std::cerr << "Error: " << what << "\n";
+void Server::create_table(const std::string &name) {
+  Guard guard(m_mutex);
+  if (m_tables.count(name)) throw OperationException("Table already exists");
+  m_tables[name] = std::make_unique<Table>(name);
 }
 
-// TODO: implement member functions
+Table *Server::find_table(const std::string &name) {
+  Guard guard(m_mutex);
+  auto it = m_tables.find(name);
+  if (it == m_tables.end()) throw OperationException("Table not found");
+  return it->second.get();
+}
+
+void Server::log_error(const std::string &what) {
+  std::cerr << "Error: " << what << std::endl;
+}
