e4658582c3fb1de8676d33421cfa245695a1389f
arianashafiee <143358685+arianashafiee@users.noreply.github.com>
2024-12-07 23:31:52 -0500

    .

--- csf_assign05/server.cpp
+++ csf_assign05/server.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+#include <thread>
+#include <map>
+#include <mutex>
+#include <memory>
+#include <cstring>
+#include "csapp.h"
+#include "server.h"
+#include "client_connection.h"
+
 #include "server.h"
 #include <iostream>
 #include <stdexcept>
@@ -50,24 +60,3 @@ void *Server::client_worker(void *arg) {
     return nullptr;
 }
 
-void Server::log_error(const std::string &what) {
-    std::cerr << "Error: " << what << std::endl;
-}
-
-void Server::create_table(const std::string &name) {
-    pthread_mutex_lock(&m_table_lock);
-    if (m_tables.find(name) != m_tables.end()) {
-        pthread_mutex_unlock(&m_table_lock);
-        throw std::runtime_error("Table already exists: " + name);
-    }
-    m_tables[name] = std::make_unique<Table>(name);
-    pthread_mutex_unlock(&m_table_lock);
-}
-
-Table *Server::find_table(const std::string &name) {
-    pthread_mutex_lock(&m_table_lock);
-    auto it = m_tables.find(name);
-    Table *table = (it != m_tables.end()) ? it->second.get() : nullptr;
-    pthread_mutex_unlock(&m_table_lock);
-    return table;
-}
