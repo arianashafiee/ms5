966cc1cc780cdcbfa6ab2b88a49dfae5e0ad1e77
arianashafiee <143358685+arianashafiee@users.noreply.github.com>
2024-12-07 23:20:59 -0500

    .

--- csf_assign05/server.h
+++ csf_assign05/server.h
@@ -3,35 +3,40 @@
 
 #include <map>
 #include <string>
-#include <memory>
 #include <pthread.h>
 #include "table.h"
 #include "client_connection.h"
 
 class Server {
 private:
-  int m_listen_fd; // File descriptor for the listening socket
-  std::map<std::string, std::unique_ptr<Table>> m_tables; // Map of table names to Table objects
-  pthread_mutex_t m_table_lock; // Mutex for synchronizing access to m_tables
+  // TODO: add member variables
 
-  // Copy constructor and assignment operator are prohibited
-  Server(const Server &) = delete;
-  Server &operator=(const Server &) = delete;
+  // copy constructor and assignment operator are prohibited
+  Server( const Server & );
+  Server &operator=( const Server & );
+  int m_listen_fd;
+
 
 public:
   Server();
   ~Server();
 
-  void listen(const std::string &port); // Start listening for client connections
-  void server_loop(); // Main server loop for accepting and handling connections
+  void listen( const std::string &port );
+  void server_loop();
 
-  static void *client_worker(void *arg); // Worker thread function for client handling
+  static void *client_worker( void *arg );
 
-  void log_error(const std::string &what); // Log error messages
+  void log_error( const std::string &what );
 
-  // Member functions for managing tables
-  void create_table(const std::string &name); // Create a new table
-  Table *find_table(const std::string &name); // Find an existing table by name
+  // TODO: add member functions
+
+  // Some suggested member functions:
+/*
+  void create_table( const std::string &name );
+  Table *find_table( const std::string &name );
+  void log_error( const std::string &what );
+*/
 };
+
 
 #endif // SERVER_H
